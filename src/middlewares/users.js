b31e750834439c1c68ebbdd61bbe570a2bb7f45b
Doron Segal <doron2402@gmail.com>
2015-03-24 22:54:31 -0700

    starting..

--- src/middlewares/users.js
+++ src/middlewares/users.js
@@ -1,13 +1,13 @@
 let mwUsers = {};
 let Joi = require('joi');
 let userSchema = Joi.object().options({ abortEarly: false }).keys({
-    email: Joi.string().email().required().label('User Email'),
-    username: Joi.string().min(6).required(),
-    password: Joi.string().min(8).required(),
-    passwordConfirmation: Joi.any().valid(Joi.ref('password')).required().options({ language: { any: { allowOnly: 'must match password' }, label: 'Password Confirmation' } }).label('This label is not used because language.label takes precedence'),
-    firstName: Joi.string().required(),
-    lastName: Joi.string().required(),
-    confirm: Joi.number().optional()
+  email: Joi.string().email().required().label('User Email'),
+  username: Joi.string().min(6).required(),
+  password: Joi.string().min(8).required(),
+  passwordConfirmation: Joi.any().valid(Joi.ref('password')).required().options({ language: { any: { allowOnly: 'must match password' }, label: 'Password Confirmation' } }).label('This label is not used because language.label takes precedence'),
+  firstName: Joi.string().required(),
+  lastName: Joi.string().required(),
+  confirm: Joi.number().optional()
 });
 
 
--- src/schemas/users.js
+++ src/schemas/users.js
@@ -1,5 +1,7 @@
 let mongoose = require('mongoose');
-let userSchema = mongoose.Schema({
+let Schema = mongoose.Schema;
+
+let userSchema = new Schema({
   firstName: String,
   lastName: String,
   username: {
